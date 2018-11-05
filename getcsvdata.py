755e3973e11360c4209883d3f3a1c08d97902a2e
mhubris <miguelandrade1996.ma@gmail.com>
2018-11-05 15:54:37 +0000

    server and client

--- getcsvdata.py
+++ getcsvdata.py
@@ -5,10 +5,11 @@ import matplotlib as plt
 import novainstrumentation as ni
 
 # open csv file
-with open('datafiles\\myfile.csv') as csvfile:
+with open('datafiles\\N001.csv') as csvfile:
     # get comma separated values from file
     data = list(csv.reader(csvfile, delimiter=';'))
 
+'''''
 # get time vector (x-axis)
 t = [float(i[0].replace(',','.')) for i in data]
 
@@ -25,5 +26,22 @@ z = [float(i[3].replace(',','.')) for i in data]
 plot(t, x)
 plot(t, y)
 plot(t, z)
+'''
+##
+
+# get X acceleration (replace comma with dot for numeric parsing)
+x = [float(i[0].replace(',','.')) for i in data]
+
+# get Y acceleration (replace comma with dot for numeric parsing)
+y = [float(i[1].replace(',','.')) for i in data]
+
+# get Z acceleration (replace comma with dot for numeric parsing)
+z = [float(i[2].replace(',','.')) for i in data]
+
+# plot accelerations in X, Y and Z
+plot(x)
+plot(y)
+plot(z)
+##
 
 plt.pyplot.show()
